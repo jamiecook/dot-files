aa39babf9cb8bfabda8938ad12e8b3c1839bd4e3
Jamie Cook <jamie@outerloop.io>
2024-05-02 12:39:10 +1000

    better clone

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -5,14 +5,23 @@ set -eu
 main() {
   cd
 
-  if [[ ! -d '.dot-files' ]]
+  if [[ ! -d "${HOME}/.dot-files" ]]
   then
-    git clone https://github.com/jamiecook/dot-files.git .dot-files
+    git clone https://github.com/jamiecook/dot-files.git ~/.dot-files
   fi
 
-  ls -1d --color=none .dot-files/files/* .dot-files/files/.* | while read f; do
-    local bname=$(basename $f)
-    [ "$bname" == '.' ] || [ "$bname" == '..' ] || [ "$bname" == '.git' ] || ln -vsF "$f" .
+  ls -1d --color=none ~/.dot-files/files/* ~/.dot-files/files/.* | while read src; do
+    local bname=$(basename ${src})
+    local dest=~/${bname}
+
+    # Skip .git and special dirs
+    ([ "$bname" == '.' ] || [ "$bname" == '..' ] || [ "$bname" == '.git' ]) && continue
+
+    if [[ -e "${dest}" ]]; then
+      echo "skipping ${dest} - already exists"
+    else
+      ln -vsF "${src}" "${dest}"
+    fi
   done
 }
 
